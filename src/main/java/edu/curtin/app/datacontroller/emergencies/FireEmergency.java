821107d2bc6fa5a05605909dce39b3e96f5a1e7a
Dilanka Seneviratne <dvseneviratne@gmail.com>
2022-05-27 00:04:55 +0530

    Add abstract methods
    Added missing abstract methods to the Emergency Type classes

--- src/main/java/edu/curtin/app/datacontroller/emergencies/FireEmergency.java
+++ src/main/java/edu/curtin/app/datacontroller/emergencies/FireEmergency.java
@@ -8,4 +8,34 @@ public class FireEmergency extends EmergencySimulator{
     public void testCase(){
         System.out.println("FIRE Method is working");
     }
+
+    @Override
+    public void start() {
+        
+    }
+
+    @Override
+    public void idle() {
+
+    }
+
+    @Override
+    public void running() {
+
+    }
+
+    @Override
+    public void high_Intensity() {
+
+    }
+
+    @Override
+    public void low_Intensity() {
+
+    }
+
+    @Override
+    public void cleaned_Up() {
+
+    }
 }
